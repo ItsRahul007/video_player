e249031642d7c5fa2a3a487ccad63601e35e599e
Rahul Ghosh <rahul@mile9.io>
2025-02-02 22:04:21 +0530

    only position is not updating in every second

--- lib/screens/play_video.dart
+++ lib/screens/play_video.dart
@@ -1,6 +1,7 @@
 import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:video_player/widgets/custom_controls.dart';
 import 'package:video_player/widgets/text.dart';
 
 class PlayVideo extends StatefulWidget {
@@ -14,8 +15,26 @@ class PlayVideo extends StatefulWidget {
 
 class _PlayVideoState extends State<PlayVideo> {
   late BetterPlayerController _betterPlayerController;
-  double? videoWidth;
-  double? videoHeight;
+
+  final BetterPlayerConfiguration _betterPlayerConfiguration =
+      BetterPlayerConfiguration(
+    fit: BoxFit.contain,
+    autoDetectFullscreenDeviceOrientation: true,
+    autoPlay: true,
+    autoDetectFullscreenAspectRatio: true,
+    fullScreenByDefault: true,
+    controlsConfiguration: BetterPlayerControlsConfiguration(
+      controlsHideTime: const Duration(milliseconds: 300),
+      controlBarHeight: 80.h,
+      playerTheme: BetterPlayerTheme.custom,
+      customControlsBuilder: (controller, onPlayerVisibilityChanged) =>
+          CustomControls(
+        controller: controller,
+        onControlsVisibilityChanged: onPlayerVisibilityChanged,
+      ),
+    ),
+    // controlsConfiguration:
+  );
 
   @override
   void initState() {
@@ -23,40 +42,16 @@ class _PlayVideoState extends State<PlayVideo> {
     _initializeVideo();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    _betterPlayerController.dispose();
-  }
-
   void _initializeVideo() {
-    BetterPlayerDataSource betterPlayerDataSource =
-        BetterPlayerDataSource(BetterPlayerDataSourceType.file, widget.path);
-
-    _betterPlayerController = BetterPlayerController(
-      BetterPlayerConfiguration(
-        fit: BoxFit.contain,
-        autoDetectFullscreenDeviceOrientation: true,
-        autoPlay: true,
-      ),
+    BetterPlayerDataSource betterPlayerDataSource = BetterPlayerDataSource(
+      BetterPlayerDataSourceType.file,
+      widget.path,
     );
 
-    _betterPlayerController.setupDataSource(betterPlayerDataSource).then((_) {
-      //! Video is initialized, now we can get the dimensions
-      Size? videoSize =
-          _betterPlayerController.videoPlayerController?.value.size;
-
-      if (videoSize != null) {
-        setState(() {
-          videoWidth = videoSize.width;
-          videoHeight = videoSize.height;
-        });
-        _betterPlayerController.enterFullScreen();
-        _betterPlayerController
-            .setOverriddenAspectRatio(videoSize.width / videoSize.height);
-        if ((videoSize.width / videoSize.height) > 0.5625) {}
-      }
-    });
+    _betterPlayerController = BetterPlayerController(
+      _betterPlayerConfiguration,
+      betterPlayerDataSource: betterPlayerDataSource,
+    );
   }
 
   @override
